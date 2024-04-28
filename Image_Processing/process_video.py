066b52df3bdd0ec962ff7468e09111cef34d631b
alrochas1 <alrochas@ucm.es>
2024-04-28 19:43:36 +0200

    Añadidos comentarios a process_video.py

--- Image_Processing/process_video.py
+++ Image_Processing/process_video.py
@@ -1,43 +1,47 @@
 # Sirve para procesar un video grabado con save_video.py. El video procesado debe estar en ./Images/{video}.avi
 # La imagen guardada tendra el mismo nombre que el video (cuidado con no sobreescribir nada)
+#
+# Esta versión realiza lo basico. Se queda con el valor maximo de la imagen.  
 
 import cv2 as cv
 import numpy as np
     
 
+# Abre el video
 ruta_archivo = input("Ingrese la ruta del archivo de video (sin extensión): ")
 cap = cv.VideoCapture(f"./Images/{ruta_archivo}.avi")
 if not cap.isOpened():
     print("Error: No se pudo abrir el archivo de video.")
     exit()
-res = [cap.get(cv.CAP_PROP_FRAME_WIDTH), cap.get(cv.CAP_PROP_FRAME_HEIGHT)]
 
+# Obtiene los parametros del video (resolucion y FPS)
+res = [cap.get(cv.CAP_PROP_FRAME_WIDTH), cap.get(cv.CAP_PROP_FRAME_HEIGHT)]
 n_frames = cap.get(cv.CAP_PROP_FRAME_COUNT)
 
+# Aqui se almacenara la imagen final
 final_image = np.zeros([int(res[1]), int(res[0]), 3])
 N = 1
 
-
+# COMIENZA EL PROCESADO
 while True:
 
-    # Capture frame-by-frame
+    # Captura frame a frame
     ret, frame = cap.read()
-
-    # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
     
-    # Operaciones
+    # Muestra el frame por pantalla (se puede comentar si molesta)
     cv.imshow('Frame del video procesado', frame)
 
+    # Se queda con el valor maximo del frame (si es el primero, no hay maximo)
     if N == 1 :
         final_image = frame
     else :
         final_image = cv.max(final_image, frame)
-
     N += 1
 
+    # Para salir (si el video acaba sale solo)
     if cv.waitKey(1) == ord('q'):
         break
 
@@ -45,6 +49,7 @@ while True:
 cap.release()
 cv.destroyAllWindows()
 
+# Muestra el resultado y guarda la imagen (usa el mismo nombre que el video)
 cv.imshow('Imagen final', final_image)
 cv.imwrite(f"./Images/{ruta_archivo}.jpg", final_image)
 
